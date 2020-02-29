61935060969d1372147d03694a95cb8d8d6bcd7d
Mubaris NK <hello@mubaris.com>
2020-02-29 21:35:26 +0530

    Add refresh button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 import data from './data.json';
 
 class App extends Component {
   state = {
     data: data
+  }
+
+  refresh() {
+    axios.post("https://api.netlify.com/build_hooks/5e5a4c4067d7da3b14426f10", {})
+      .then((res) => {
+        console.log("Successful Refresh", res.data);
+      })
+      .catch((err) => {
+        console.log("Error while refreshing", err);
+      })
   }
 
   render() {
@@ -15,6 +26,7 @@ class App extends Component {
           )}
         </ul>
           <span>Last Updated - {this.state.data.date}</span>
+          <button onClick={this.refresh.bind(this)}>Refresh</button>
       </div>
     );
   }
